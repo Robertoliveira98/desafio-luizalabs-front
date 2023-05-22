36df26091d441e3eb11e30d34f95ab8a7e3feb3d
Roberto de Oliveira Faria <robertoliveirafaria@gmail.com>
2023-05-22 02:13:09 -0300

    criando integração cadastro de usuarios

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -1,9 +1,42 @@
-import { Label, TextInput, Checkbox, Button, Card, Alert } from "flowbite-react";
+import { Label, TextInput, Button, Alert, Spinner } from "flowbite-react";
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import React, { useState } from "react";
+import apiClient from "../../services/api";
+import { useRouter } from "next/router";
+import Card from "@/components/Card";
+import Divider from "@/components/Divider";
 
 const Cadastro = (props) => {
+  let [nome, setNome] = useState();
+  let [senha, setSenha] = useState();
+  let [confirmaSenha, setConfirmaSenha] = useState();
+  let [email, setEmail] = useState();
+  let [carregando, setCarregando] = useState(false);
+  let [mensagemErro, setMensagemErro] = useState("Ocorreu um erro!");
+  let [erro, setErro] = useState(false);
+  let router = useRouter(); 
+
+  const enviaCadastro = (event) => {
+    setCarregando(true);
+
+    apiClient
+      .post(`/usuario/cadastro`, { nome, senha, email })
+      .then((response) => {
+        setCarregando(false);
+        // sessionStorage.setItem("token", response.data.token)
+        debugger;
+        router.push("/home/" + response.data.token);
+      })
+      .catch((error) => {
+        setErro(true);
+        setCarregando(false);
+        setMensagemErro(error.response.data.mensagem ? error.response.data.mensagem : "Ocorreu um erro");
+      });
+    event.preventDefault();
+  };
+
+
   return (
     <>
       <Head>
@@ -13,13 +46,7 @@ const Cadastro = (props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <Card className="container mx-auto">
-          <Alert color="failure" withBorderAccent={true}> 
-            <span>
-              <span className="font-medium">Atenção! </span> 
-              E-mail já cadastrado.
-            </span>
-          </Alert>
+        <Card>
           <form className="flex flex-col gap-4">
             <div>
               <div className="mb-2 block">
@@ -29,6 +56,8 @@ const Cadastro = (props) => {
                 id="name"
                 type="text"
                 placeholder="Seu nome"
+                value={nome}
+                onChange={(e) => setNome(e.target.value)}
                 required={true}
                 shadow={true}
               />
@@ -43,6 +72,8 @@ const Cadastro = (props) => {
                 placeholder="name@gmail.com"
                 required={true}
                 shadow={true}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div>
@@ -54,6 +85,8 @@ const Cadastro = (props) => {
                 type="password"
                 required={true}
                 shadow={true}
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
               />
             </div>
             <div>
@@ -65,9 +98,28 @@ const Cadastro = (props) => {
                 type="password"
                 required={true}
                 shadow={true}
+                value={confirmaSenha}
+                onChange={(e) => setConfirmaSenha(e.target.value)}
               />
             </div>
-            <Button type="submit">Cadastrar conta</Button>
+            {erro ? 
+            <Alert className="mt-2" color="failure">
+              <span>
+                <span className="font-medium">{mensagemErro}</span>
+              </span> 
+            </Alert> : undefined
+            }
+            <Divider/>
+            <Button
+              size="lg" 
+              gradientMonochrome="info" 
+              pill={true} 
+              onClick={(e) => enviaCadastro(e)} 
+              type="submit"
+              >
+              {carregando ? <div className="pr-3"><Spinner/></div> : null}
+              Cadastrar conta
+            </Button>
           </form>
         </Card>
       </main>
